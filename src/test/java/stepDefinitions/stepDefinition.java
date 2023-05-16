3d755bef072b982859553c88fa0eb220360f1fa2
Dariia <dariia.vengrzhanovska@bequalified.de>
2023-05-16 20:24:01 +0200

    start prj

---
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -0,0 +1,30 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepDefinition {
+    @Given("User is on NetBanking landing page")
+    public void user_is_on_net_banking_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Hello");
+    }
+    @When("User login into application with username and password")
+    public void user_login_into_application_with_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Home page is populated")
+    public void home_page_is_populated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Cards are displayed")
+    public void cards_are_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
